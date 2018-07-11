72232644bbd6fa0deb009b17bfba610ecc7b3652
Ollin Langle <ollin.langle@ciencias.unam.mx>
2018-07-11 10:34:54 -0500

    auto conf for vim

---
+++ install.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+mkdir -p ~/.config/nvim
+echo "set runtimepath^=~/.vim runtimepath+=~/.vim/after
+let &packpath = &runtimepath
+source ~/.vimrc" > ~/.config/nvim/init.vim
+
+curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > installer.sh
+sh ./installer.sh ~/.config/nvim/bundles
+
+cp .vimrc $HOME/.vimrc
